dc2ba572197a0f033c12699cdc989846ab5a2a27
Moustafa Sokkar <mostafasookar@hotmail.com>
2025-10-01 21:07:36 +0400

    refine secret module to randome secret

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -2,14 +2,20 @@
 # Secrets for pgAdmin Login #
 #############################
 
+# Always create a random suffix
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+}
+
 resource "aws_secretsmanager_secret" "pgadmin" {
-  name        = "${var.name}-credentials"
+  # For prod/uat → fixed name
+  # For test → you can pass a flag later, but by default just use suffix
+  name        = "${var.name}-credentials-${random_string.suffix.result}"
   description = "Credentials for pgAdmin default login"
   tags        = merge(local.common_tags, { Name = "${var.name}-secrets" })
 }
 
-# Initial secret value (email + password)
-# In practice, you might rotate these manually or via pipeline later
 resource "aws_secretsmanager_secret_version" "pgadmin" {
   secret_id     = aws_secretsmanager_secret.pgadmin.id
   secret_string = jsonencode({
